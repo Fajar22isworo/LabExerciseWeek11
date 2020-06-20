eeceb95ff215c3bf60ec863f5a925249a40dbd32
Khias Nurlatif Ari Subekti <57320303+qazolatif@users.noreply.github.com>
2020-06-20 17:04:37 +0700

    Update Karyawan.cs

--- BaseClass(Parent)/Karyawan.cs
+++ BaseClass(Parent)/Karyawan.cs
@@ -10,12 +10,6 @@ namespace LabExerciseWeek11.EmployeeData
 		public string NIK {get;set;}
 		public string Nama {get;set;}
         
-        	public Karyawan(string nik="null", string nama="null")
-       		{
-       			this.NIK = nik;
-           		this.Nama = nama;
-      		}
-        
-	public abstract double Gaji();
+		public abstract double Gaji();
 	}
 }
